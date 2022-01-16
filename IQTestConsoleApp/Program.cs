9e4d8e335648f6de2f12686df4251f896cae492b
Michael Vogel <ipbw@mail.ru>
2022-01-16 13:27:47 +0300

    Added class Question

--- IQTestConsoleApp/Program.cs
+++ IQTestConsoleApp/Program.cs
@@ -1,9 +1,58 @@
-﻿namespace IQTestConsoleApp;
+﻿using System.Text;
+
+namespace IQTestConsoleApp;
 
 class Program
 {
     static void Main()
     {
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.InputEncoding = Encoding.Unicode;
 
+        var questions = GetQuestions();
+        var countRightAnswer = 0;
+        bool isValid;
+
+        var random = new Random();
+
+        for (int i = 0; i < questions.Count; i++)
+        {
+            Console.WriteLine($"Вопрос № {i + 1}");
+
+            var randomIndex = random.Next(questions.Count);
+            Console.WriteLine(questions[randomIndex].QuestionText);
+
+            do
+            {
+                isValid = int.TryParse(Console.ReadLine(), out var userAnswer);
+                if (!isValid || userAnswer < 0)
+                {
+                    Console.WriteLine("Неправильный ввод. \nВведите цифры!");
+                    isValid = false;
+                }
+                else
+                {
+                    if (userAnswer == questions[i].Answer)
+                    {
+                        countRightAnswer++;
+                    }
+                }
+            }
+            while (!isValid);
+            questions.RemoveAt(randomIndex);
+        }
+        Console.WriteLine($"Количество правильных ответов: {countRightAnswer}");
+
+    }
+    static List<Question> GetQuestions()
+    {
+        List<Question> questions = new();
+        questions.Add(new Question("Сколько будет два плюс два умноженное на два?", 6));
+        questions.Add(new Question("Сколько будет два плюс два умноженное на два?", 9));
+        questions.Add(new Question("Бревно нужно распилить на 10 частей, сколько надо сделать распилов?", 9));
+        questions.Add(new Question("На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25));
+        questions.Add(new Question("Укол делают каждые полчаса, сколько нужно минут для трех уколов?", 60));
+        questions.Add(new Question("Пять свечей горело, две потухли. Сколько свечей осталось?", 2));
+        return questions;
     }
 }
---
+++ IQTestConsoleApp/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IQTestConsoleApp
+{
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        public int Answer { get; set; }
+
+        public Question (string text, int answer)
+        {
+            QuestionText = text;
+            Answer = answer;
+        }
+        
+    }
+}
